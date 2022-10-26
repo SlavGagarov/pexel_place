d5c7a2c456f1609f2627a4453b41573f9c3c1bb8
SLAV GAGAROV <slavgagarov@gmail.com>
2022-10-25 21:50:23 -0500

    fixed create_cells migration

--- db/migrate/20221026011102_create_cells.rb
+++ db/migrate/20221026011102_create_cells.rb
@@ -1,9 +1,9 @@
 class CreateCells < ActiveRecord::Migration[7.0]
   def change
     create_table :cells do |t|
-      t.x :integer
-      t.y :integer
-      t.color :string
+      t.integer :x
+      t.integer :y
+      t.string :color
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_004244) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_26_011102) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cells", force: :cascade do |t|
+    t.integer "x"
+    t.integer "y"
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "plants", force: :cascade do |t|
     t.string "name"
