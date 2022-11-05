35decce83ac403993a5f81f47e7a33ddb2777ad1
SLAV GAGAROV <slavgagarov@gmail.com>
2022-11-05 17:25:37 -0500

    added color menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+addOnClicks()
+
+function addOnClicks() {
+  let cells = document.getElementsByClassName("cell_button")
+  
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", showColorMenu)
+  }
+}
+
+function showColorMenu() {
+  document.getElementById("color_menu").style.display = "block"
+}
